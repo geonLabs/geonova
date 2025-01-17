5284efde75e11eb9134e6521357347dc65bf2817
dbparkJ <pjmsm0319@naver.com>
2025-01-17 09:21:16 +0900

    change using img_lib

--- src/geonova/script/utils/geonova.py
+++ src/geonova/script/utils/geonova.py
@@ -9,6 +9,7 @@ import time
 
 class Geonova:
     def __init__(self):
+        self.img_lib = rospy.get_param("img_lib")
         self.model_path = rospy.get_param("model", "../models/yolov8n.pt")
         self.rgb_img_sub = Subscriber("/oak/rgb/image_raw", Image)
         self.stereo_img_sub = Subscriber("/oak/stereo/image_raw", Image)
@@ -24,25 +25,22 @@ class Geonova:
             slop=0.1)
 
         ats.registerCallback(self.sync_callback)
-
         rospy.spin()
 
     def sync_callback(self, rgb_msg, stereo_msg):
         model = utils.yolo_models.model_load(self.model_path)
 
-        rgb_image = self.iamgetools.convert_image(rgb_msg)
-        # rgb_image = self.iamgetools.ros_image_to_numpy(rgb_msg)
-        stereo_image = self.iamgetools.convert_image(stereo_msg)
-        
-        # start_time = time.time()
-        resize_img = self.iamgetools.resize_with_padding(rgb_image)
-        # rgb_vpi_img = self.iamgetools.flip_img(self.iamgetools.numpy_to_vpi_image(rgb_image))
-        # resize_img = self.iamgetools.add_padding_to_original(rgb_vpi_img, 640, 640)
-        # tensor_img = self.iamgetools.vpi_to_torch_tensor(resize_img)
-        # end_time = time.time()
-        # rospy.loginfo(f"Resize and padding processing time: {(end_time - start_time) * 1000:.2f} ms")
+        if self.img_lib == "opencv":
+            rgb_image = self.iamgetools.convert_image(rgb_msg)
+            resize_img = self.iamgetools.resize_with_padding(rgb_image)
+        else :
+            rgb_image = self.iamgetools.ros_image_to_numpy(rgb_msg)
+            rgb_vpi_img = self.iamgetools.flip_img(self.iamgetools.numpy_to_vpi_image(rgb_image))
+            rgb_vpi_resize = self.iamgetools.add_padding_to_original(rgb_vpi_img, 640, 640)
+            resize_img = self.iamgetools.vpi_to_torch_tensor(rgb_vpi_resize)
 
-        model(resize_img, verbose=False)
+        model(resize_img)
+        stereo_image = self.iamgetools.convert_image(stereo_msg)
 
     def stereo(self):
         pass
